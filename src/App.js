330bf3882f633766a933552bfe1b682f766a2ca6
jeongyongman <i1683u@gmail.com>
2021-10-25 08:37:08 +0900

    15.2 state 사용

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import Subject from './components/Subject';
 
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:"WEB", sub:"world wide web!"}
+    }
+
+  }
   render(){
     return(
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML은 하이퍼텍스트 마크업 랭귀지이다."></Content>
       </div>
