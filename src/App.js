e3facfd791ea1d8b4cb679b9d77065448c8f88a5
jeongyongman <i1683u@gmail.com>
2021-10-25 11:34:00 +0900

    15.3 key

--- src/App.js
+++ src/App.js
@@ -9,9 +9,13 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      subject:{title:"WEB", sub:"world wide web!"}
+      subject:{title:"WEB", sub:"world wide web!"},
+      contents:[
+        {id:1,title:'HTML',desc:'HTML is for information'},
+        {id:2,title:'CSS',desc:'CSS is for design'},
+        {id:3,title:'JavaScript',desc:'JavaScript is for interactive'},
+      ]
     }
-
   }
   render(){
     return(
@@ -20,7 +24,7 @@ class App extends Component{
           title={this.state.subject.title} 
           sub={this.state.subject.sub}>
         </Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML은 하이퍼텍스트 마크업 랭귀지이다."></Content>
       </div>
     );
